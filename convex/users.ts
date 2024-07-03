edca65bbb92285b7dd0b70e1e51ca24f452f8c8d
Shubhankargupta691 <shubhankargupta691@gmail.com>
2024-07-04 02:36:35 +0530

    update:users

--- convex/users.ts
+++ convex/users.ts
@@ -1,21 +1,76 @@
-import {v } from "convex/values";
-import { internalMutation } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
 
+import { internalMutation, query } from "./_generated/server";
 
-export const createUsers = internalMutation({
-    args:{
-        clerkId: v.string(),
-        email: v.string(),
-        imageUrl: v.string(),
-        name: v.string(),
+export const getUserById = query({
+  args: { clerkId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .unique();
 
-    }, handler: async(ctx, args) => {
-        await ctx.db.insert('users',{
-            clerkId: args.clerkId,
-            email: args.email,
-            imageUrl: args.imageUrl,
-            name: args.name,
-            
-        })
+    if (!user) {
+      throw new ConvexError("User not found");
     }
-})
+
+    return user;
+  },
+});
+
+
+export const getTopUserByPodcastCount = query({
+  args: {},
+  handler: async (ctx, args) => {
+    const user = await ctx.db.query("users").collect();
+
+    const userData = await Promise.all(
+      user.map(async (u) => {
+        const podcasts = await ctx.db
+          .query("podcasts")
+          .filter((q) => q.eq(q.field("authorId"), u.clerkId))
+          .collect();
+
+        const sortedPodcasts = podcasts.sort((a, b) => b.views - a.views);
+
+        return {
+          ...u,
+          totalPodcasts: podcasts.length,
+          podcast: sortedPodcasts.map((p) => ({
+            podcastTitle: p.podcastTitle,
+            podcastId: p._id,
+          })),
+        };
+      })
+    );
+
+    return userData.sort((a, b) => b.totalPodcasts - a.totalPodcasts);
+  },
+});
+
+export const createUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    imageUrl: v.string(),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+  },
+});
+
+export const updateUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    imageUrl: v.string(),
+    email: v.string(),
+  },
+  async handler(ctx, args) {
+  },
+});
+
+export const deleteUser = internalMutation({
+  args: { clerkId: v.string() },
+  async handler(ctx, args) {
+  },
+});
