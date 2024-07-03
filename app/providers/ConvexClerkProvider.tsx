6f0d8ece39cb7b58fa8be4e973d32515f886847f
Shubhankargupta691 <shubhankargupta691@gmail.com>
2024-07-04 04:22:15 +0530

    fix:error

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -1,14 +1,30 @@
 'use client';
 
 import React, { ReactNode } from 'react';
-import { ClerkProvider, useAuth } from '@clerk/clerk-react';
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ConvexReactClient } from 'convex/react';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
-  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+    appearance={{
+      layout: { 
+        socialButtonsVariant: 'iconButton',
+        logoImageUrl: '/icons/auth-logo.svg'
+      },
+      variables: {
+        colorBackground: '#15171c',
+        colorPrimary: '',
+        colorText: 'white',
+        colorInputBackground: '#1b1f29',
+        colorInputText: 'white',
+        colorShimmer: 'white',
+
+      }
+    }}
+  >
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
     </ConvexProviderWithClerk>
