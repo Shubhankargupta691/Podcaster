335322baf2bf4d6fe626e0ffa106cb77fb08e203
Shubhankargupta691 <shubhankargupta691@gmail.com>
2024-07-04 04:21:48 +0530

    fix:error

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,16 @@
+import Image from "next/image";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-          <main>
-              {children}
-          </main>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="relative h-screen w-full">
+      <div className="absolute inset-0">
+        <Image src="/images/bg-img.png" alt="background" layout="fill" className="object-cover" />
+      </div>
+      <div className="relative z-10">{children}</div>
+    </main>
+  );
+}
